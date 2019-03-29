34f5694d236d6f68ed762bce08d88ae24110896d
heplayskeys <heplayskeys@gmail.com>
2019-03-29 12:08:06 -0700

    Updates

---
+++ precache-manifest.3624485a882b8f31636ff842793333a3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "40bb0c8c2e3a4a70d3d7",
+    "url": "/clickster/static/js/runtime~main.9928b7d5.js"
+  },
+  {
+    "revision": "b17bb9699b612d7e7830",
+    "url": "/clickster/static/js/main.8e350be7.chunk.js"
+  },
+  {
+    "revision": "5b8bbdbb75b8b44c442b",
+    "url": "/clickster/static/js/2.6fcf23cc.chunk.js"
+  },
+  {
+    "revision": "b17bb9699b612d7e7830",
+    "url": "/clickster/static/css/main.53aac47c.chunk.css"
+  },
+  {
+    "revision": "287ccc494f827bf8c09a18f185062a16",
+    "url": "/clickster/index.html"
+  }
+];
