bb8581fb2b51ba994ad97b20d2fb1e23dbcc5037
heplayskeys <heplayskeys@gmail.com>
2019-03-30 23:07:05 -0700

    Updates

--- precache-manifest.573021828bbeda34d14de30bd6527c8b.js
+++ precache-manifest.573021828bbeda34d14de30bd6527c8b.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/clickster/static/js/runtime~main.9928b7d5.js"
   },
   {
-    "revision": "d27a9369fdf42d8f4329",
-    "url": "/clickster/static/js/main.97e569df.chunk.js"
+    "revision": "6fa9d886b8823a89b3bb",
+    "url": "/clickster/static/js/main.2bdae942.chunk.js"
   },
   {
     "revision": "5b8bbdbb75b8b44c442b",
     "url": "/clickster/static/js/2.6fcf23cc.chunk.js"
   },
   {
-    "revision": "d27a9369fdf42d8f4329",
-    "url": "/clickster/static/css/main.4cb51d58.chunk.css"
+    "revision": "6fa9d886b8823a89b3bb",
+    "url": "/clickster/static/css/main.3985971b.chunk.css"
   },
   {
-    "revision": "aecbef61d5be832112acdf3f739d8959",
+    "revision": "195f1b4fdb533abeea244e0af4ea4234",
     "url": "/clickster/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/clickster/precache-manifest.3079357d737af4768b38b3bed9996bdc.js"
+  "/clickster/precache-manifest.573021828bbeda34d14de30bd6527c8b.js"
 );
 
 workbox.clientsClaim();
